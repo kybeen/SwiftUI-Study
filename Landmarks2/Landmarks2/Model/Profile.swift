f40fc271ef63cdf955a36758d203f5b0d219c6f0
kybeen <rei980627@gmail.com>
2023-06-29 16:22:06 +0900

    Landmarks2 - 튜토리얼 복습

---
+++ Landmarks2/Landmarks2/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Landmarks2
+//
+//  Created by 김영빈 on 2023/06/29.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "kybeen")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
---
+++ Landmarks2/Landmarks2/Views/Hikes/HikeBadge.swift
@@ -0,0 +1,30 @@
+//
+//  HikeBadge.swift
+//  Landmarks2
+//
+//  Created by 김영빈 on 2023/06/29.
+//
+
+import SwiftUI
+
+struct HikeBadge: View {
+    var name: String
+    
+    var body: some View {
+        VStack(alignment: .center) {
+            Badge()
+                .frame(width: 300, height: 300)
+                .scaleEffect(1.0 / 3.0)
+                .frame(width: 100, height: 100)
+            Text(name)
+                .font(.caption)
+                .accessibilityLabel("Badge for \(name).")
+        }
+    }
+}
+
+struct HikeBadge_Previews: PreviewProvider {
+    static var previews: some View {
+        HikeBadge(name: "Preview Testing")
+    }
+}
